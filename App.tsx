2125d84bd981aadc424a53f17ca041c9886c5869
Paula Rolim <rolim.paula1999@gmail.com>
2023-03-03 10:43:04 -0300

    feat: clear project

--- App.tsx
+++ App.tsx
@@ -1,63 +1,13 @@
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import { Section } from './src/Section';
-
-const styles = StyleSheet.create({
-  highlight: { fontWeight: '700' },
-});
+import { Box, NativeBaseProvider } from 'native-base';
+import { Home } from './src/screens/Home';
+import { theme } from './src/styles/theme';
 
 export function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}
-      >
-        <Header />
-        <View
-          style={{ backgroundColor: isDarkMode ? Colors.black : Colors.white }}
-        >
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NativeBaseProvider theme={theme}>
+      <Box flex={1} bg={theme.colors.primary[100]}>
+        <Home />
+      </Box>
+    </NativeBaseProvider>
   );
 }
